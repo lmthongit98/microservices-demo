d1757f52947dd8002b5614f95a4fd95429314149
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2023-04-07 22:49:56 +0700

    auth service

---
+++ auth-service/src/main/java/com/tma/authservice/service/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.tma.authservice.service;
+
+import com.tma.authservice.entity.User;
+import com.tma.authservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not be found");
+        }
+        List<SimpleGrantedAuthority> authorities = user.getRoles().stream().map(SimpleGrantedAuthority::new).toList();
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
+
+}
