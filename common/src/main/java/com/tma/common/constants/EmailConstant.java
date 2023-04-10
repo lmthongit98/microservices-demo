89deb5b51c5f9d2b7adf55fff72ac74235764df4
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2023-04-11 00:06:32 +0700

    change from email

--- common/src/main/java/com/tma/common/constants/EmailConstant.java
+++ common/src/main/java/com/tma/common/constants/EmailConstant.java
@@ -1,7 +1,7 @@
 package com.tma.common.constants;
 
 public class EmailConstant {
-    public static final String NO_REPLY = "noreply@gmail.com";
+    public static final String NO_REPLY = "lmthong98@gmail.com";
     public static final String ORDER_EMAIL_TEMPLATE = "order-email";
     public static final String ORDER_EMAIL_SUBJECT = "Order {} was placed successfully";
     public static final String REGISTER_USER_EMAIL_SUBJECT = "Registered account successfully";
